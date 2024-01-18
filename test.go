eebf0d30d7a77bfde43c793b5363dbf1d37c88cc
dheker <dhekerlaadhibii@gmail.com>
2024-01-18 22:27:48 +0100

    defining the role of each line

--- test.go
+++ test.go
@@ -1,7 +1,10 @@
+
+///starting point for Go compiler
 package main
 // fmt is package that provides function for printing text 
 import "fmt"
 
 func main() {
+    // print hello world
     fmt.Println("Hello, World!")
 }
