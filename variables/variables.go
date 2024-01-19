c92eb2bf0e32f843da12127bc6678be8fcd0cb45
dheker <dhekerlaadhibii@gmail.com>
2024-01-19 23:27:24 +0100

    deal with iota

--- variables/variables.go
+++ variables/variables.go
@@ -7,6 +7,18 @@ var favNumb int
 const pi = 3.14
 var score float32 = 5.0
 
+//iota 
+/*
+The iota keyword in Go is particularly useful when you want to create
+ a sequence of related constant values with a pattern. Here are some common use cases
+ for iota:
+ */
+ // to start with 1 change  the iota keyword  to iota+1
+ const (
+    Red = iota
+    Green
+    Blue
+)
 func main() {
     otherFunction()
 }
@@ -19,4 +31,5 @@ func otherFunction() {
     fmt.Println(favNumb)
     fmt.Println(score)
     fmt.Println("declared variable inside function", myage)
+    fmt.Println(Red , Green,Blue)
 }
