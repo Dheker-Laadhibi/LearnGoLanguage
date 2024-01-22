551d124190ab9763aa30f3917fdbe9afe73e9d94
dheker <dhekerlaadhibii@gmail.com>
2024-01-22 23:26:42 +0100

    structs

---
+++ src/github.com/Dheker-Laadhibi/tutorial1/s1_basics/structs/structs.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+  // declaring struct
+  type Employee struct {
+	name string
+	age  int
+}
+  // new emp constructor
+  func Newemployee(name string, age int) *Employee {
+	return &Employee{name: name, age: age}
+}
+// unlike traditional object-oriented programming, go doesn't have a class object architecture rather
+    /*
+        we have structs
+    */
+
+func main() {
+    fmt.Println("what is structs in go ")
+    
+  emp:= &Employee{"algo",24}
+fmt.Println("first empolyee : \n" , emp)
+fmt.Println("employee age  : \n" ,emp.age)
+fmt.Println("employee name : \n" , emp.name)
+//type of employee
+fmt.Printf("Type of emp: %T\n", emp)
+
+}
