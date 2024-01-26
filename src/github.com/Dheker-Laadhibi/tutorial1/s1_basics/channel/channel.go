81b8d61e880331c749dda86a36770a0a47d49239
dheker <dhekerlaadhibii@gmail.com>
2024-01-26 15:59:00 +0100

    declaring a ready to use channel

--- src/github.com/Dheker-Laadhibi/tutorial1/s1_basics/channel/channel.go
+++ src/github.com/Dheker-Laadhibi/tutorial1/s1_basics/channel/channel.go
@@ -25,6 +25,15 @@ func main() {
 	f.Println("Hello World")
 // declaring channel 
 // printing type and value 
-	var c chan int
+// nil channel is not useful you can not use to read data or pass data from a channel which is nil
+/*	
+var c chan int
 	f.Printf("channe; value %v\n   type %T\n", c , c) 
+// nil value  type chan int 
+	*/
+
+	// declaring a ready to use channel 
+	c1:= make(chan string)
+	// value is an adress memory 
+	f.Printf("channel : value :  %v\n type %T\n", c1, c1)
 }
